7c9215ac5e11aa35a97eb2f3b78709f5ce2e6aac
jamie.hess <jamie.hess@MJ035SUC>
2016-05-26 16:16:29 -0500

    Add project files.

---
+++ MouseClicker/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿using System;
+
+namespace MouseClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.durationOfClicks = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clickSpeed = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblTotalClicks = new System.Windows.Forms.Label();
+            this.timerClickSpeed = new System.Windows.Forms.Timer(this.components);
+            this.timerDuration = new System.Windows.Forms.Timer(this.components);
+            this.timerCountDown = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.durationOfClicks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clickSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Duration (s)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // durationOfClicks
+            // 
+            this.durationOfClicks.Location = new System.Drawing.Point(118, 86);
+            this.durationOfClicks.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.durationOfClicks.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.durationOfClicks.Name = "durationOfClicks";
+            this.durationOfClicks.Size = new System.Drawing.Size(120, 20);
+            this.durationOfClicks.TabIndex = 3;
+            this.durationOfClicks.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(283, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 188);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Click Area";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Click Speed (ms)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // clickSpeed
+            // 
+            this.clickSpeed.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.clickSpeed.Location = new System.Drawing.Point(118, 121);
+            this.clickSpeed.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.clickSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.clickSpeed.Name = "clickSpeed";
+            this.clickSpeed.Size = new System.Drawing.Size(120, 20);
+            this.clickSpeed.TabIndex = 7;
+            this.clickSpeed.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Calculate Clicks";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblTotalClicks
+            // 
+            this.lblTotalClicks.AutoSize = true;
+            this.lblTotalClicks.Location = new System.Drawing.Point(118, 22);
+            this.lblTotalClicks.Name = "lblTotalClicks";
+            this.lblTotalClicks.Size = new System.Drawing.Size(13, 13);
+            this.lblTotalClicks.TabIndex = 9;
+            this.lblTotalClicks.Text = "1";
+            // 
+            // timerClickSpeed
+            // 
+            this.timerClickSpeed.Interval = 1000;
+            this.timerClickSpeed.Tick += new System.EventHandler(this.timerClickSpeed_Tick);
+            // 
+            // timerDuration
+            // 
+            this.timerDuration.Interval = 1000;
+            this.timerDuration.Tick += new System.EventHandler(this.timerDuration_Tick);
+            // 
+            // timerCountDown
+            // 
+            this.timerCountDown.Interval = 5000;
+            this.timerCountDown.Tick += new System.EventHandler(this.timerCountDown_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 262);
+            this.Controls.Add(this.lblTotalClicks);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.clickSpeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.durationOfClicks);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Crazy Clicker";
+            ((System.ComponentModel.ISupportInitialize)(this.durationOfClicks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clickSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown durationOfClicks;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown clickSpeed;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblTotalClicks;
+        private System.Windows.Forms.Timer timerClickSpeed;
+        private System.Windows.Forms.Timer timerDuration;
+        private System.Windows.Forms.Timer timerCountDown;
+    }
+}
+
---
+++ MouseClicker/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace MouseClicker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, int dx, int dy, uint cButtons, int dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        private System.Timers.Timer _delayTimer;
+        private System.Timers.Timer _clickTimer;
+
+        private int _numberOfSeconds;
+        private int _clickSpeed;
+        private int _sumClicks;
+
+
+        delegate void SetTextCallback(string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetValues();
+
+            _delayTimer = new System.Timers.Timer();
+            _delayTimer.Interval = 1000;
+            _delayTimer.Elapsed += _delayTimer_Elapsed;
+
+            _clickTimer = new System.Timers.Timer();
+            _clickTimer.Interval = _clickSpeed;
+            _clickTimer.Elapsed += _clickTimer_Elapsed;
+
+            _delayTimer.Start();
+        }
+
+        private void _clickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DoMouseClick();
+        }
+
+        private void _delayTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //button1.Text = _numberOfSeconds.ToString();
+
+            this.SetText(_numberOfSeconds.ToString());
+
+            if (_numberOfSeconds == 0)
+            {
+                _delayTimer.Stop();
+                this.SetText("Start");
+                _clickTimer.Start();
+            }
+
+            --_numberOfSeconds;        
+        }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.button1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.button1.Text = text;
+            }
+        }
+
+
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            if (_clickSpeed == 0)
+            {
+                _clickTimer.Stop();
+            }
+            else
+            {
+                int X = Cursor.Position.X;
+                int Y = Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+
+                _clickSpeed = _clickSpeed - (int)clickSpeed.Value;
+            }
+            //            MessageBox.Show("ClickTimer Triggered");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.BackColor == Color.Red)
+            {
+                button2.BackColor = Color.Green;
+            }
+            else
+            {
+                button2.BackColor = Color.Red;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetValues();
+            lblTotalClicks.Text = _sumClicks.ToString();
+        }
+
+        private void SetValues()
+        {
+            _clickSpeed = (int)clickSpeed.Value;
+            _numberOfSeconds = (int)durationOfClicks.Value;
+            _sumClicks = (_numberOfSeconds * 1000) / _clickSpeed;
+
+            timerClickSpeed.Interval = _clickSpeed;
+            timerDuration.Interval = _numberOfSeconds;
+        }
+
+        private void timerDuration_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerClickSpeed_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerCountDown_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
