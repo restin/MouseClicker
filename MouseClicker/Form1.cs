f4305afe66b92f6b95c6d0742bf9397521719ab3
Restin <grift3r1@hotmail.com>
2016-05-27 09:28:14 -0500

    Latest Updates

--- MouseClicker/Form1.cs
+++ MouseClicker/Form1.cs
@@ -39,71 +39,77 @@ namespace MouseClicker
         {
             SetValues();
 
-            _delayTimer = new System.Timers.Timer();
-            _delayTimer.Interval = 1000;
-            _delayTimer.Elapsed += _delayTimer_Elapsed;
+            //_delayTimer = new System.Timers.Timer();
+            //_delayTimer.Interval = 1000;
+            //_delayTimer.Elapsed += _delayTimer_Elapsed;
 
-            _clickTimer = new System.Timers.Timer();
-            _clickTimer.Interval = _clickSpeed;
-            _clickTimer.Elapsed += _clickTimer_Elapsed;
+            //_clickTimer = new System.Timers.Timer();
+            //_clickTimer.Interval = _clickSpeed;
+            //_clickTimer.Elapsed += _clickTimer_Elapsed;
 
-            _delayTimer.Start();
+            //_delayTimer.Start();
+
+            timerCountDown.Start();
         }
 
-        private void _clickTimer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            DoMouseClick();
-        }
+        //private void _clickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        //{
+        //    DoMouseClick();
+        //}
 
-        private void _delayTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            //button1.Text = _numberOfSeconds.ToString();
+        //private void _delayTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        //{
+        //    //button1.Text = _numberOfSeconds.ToString();
 
-            this.SetText(_numberOfSeconds.ToString());
+        //    this.SetText(_numberOfSeconds.ToString());
 
-            if (_numberOfSeconds == 0)
-            {
-                _delayTimer.Stop();
-                this.SetText("Start");
-                _clickTimer.Start();
-            }
+        //    if (_numberOfSeconds == 0)
+        //    {
+        //        _delayTimer.Stop();
+        //        this.SetText("Start");
+        //        _clickTimer.Start();
+        //    }
 
-            --_numberOfSeconds;        
-        }
+        //    --_numberOfSeconds;        
+        //}
 
-        private void SetText(string text)
-        {
-            // InvokeRequired required compares the thread ID of the
-            // calling thread to the thread ID of the creating thread.
-            // If these threads are different, it returns true.
-            if (this.button1.InvokeRequired)
-            {
-                SetTextCallback d = new SetTextCallback(SetText);
-                this.Invoke(d, new object[] { text });
-            }
-            else
-            {
-                this.button1.Text = text;
-            }
-        }
+        //private void SetText(string text)
+        //{
+        //    // InvokeRequired required compares the thread ID of the
+        //    // calling thread to the thread ID of the creating thread.
+        //    // If these threads are different, it returns true.
+        //    if (this.button1.InvokeRequired)
+        //    {
+        //        SetTextCallback d = new SetTextCallback(SetText);
+        //        this.Invoke(d, new object[] { text });
+        //    }
+        //    else
+        //    {
+        //        this.button1.Text = text;
+        //    }
+        //}
 
 
         public void DoMouseClick()
         {
             //Call the imported function with the cursor's current position
-            if (_clickSpeed == 0)
-            {
-                _clickTimer.Stop();
-            }
-            else
-            {
-                int X = Cursor.Position.X;
-                int Y = Cursor.Position.Y;
-                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+            //if (_clickSpeed == 0)
+            //{
+            //    _clickTimer.Stop();
+            //}
+            //else
+            //{
+            //    int X = Cursor.Position.X;
+            //    int Y = Cursor.Position.Y;
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
 
-                _clickSpeed = _clickSpeed - (int)clickSpeed.Value;
-            }
+            //    _clickSpeed = _clickSpeed - (int)clickSpeed.Value;
+            //}
             //            MessageBox.Show("ClickTimer Triggered");
+
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
 
         }
 
@@ -128,7 +134,7 @@ namespace MouseClicker
         private void SetValues()
         {
             _clickSpeed = (int)clickSpeed.Value;
-            _numberOfSeconds = (int)durationOfClicks.Value;
+            _numberOfSeconds = 5;
             _sumClicks = (_numberOfSeconds * 1000) / _clickSpeed;
 
             timerClickSpeed.Interval = _clickSpeed;
@@ -137,17 +143,35 @@ namespace MouseClicker
 
         private void timerDuration_Tick(object sender, EventArgs e)
         {
+            if (_clickSpeed != 0)
+            {
+                timerClickSpeed.Start();
+                _clickSpeed = _clickSpeed - (int)clickSpeed.Value;
+            }
+            else
+            {
+                timerClickSpeed.Stop();
+                timerDuration.Stop();
+            }
 
         }
 
         private void timerClickSpeed_Tick(object sender, EventArgs e)
         {
-
+            DoMouseClick();
         }
 
         private void timerCountDown_Tick(object sender, EventArgs e)
         {
+            button1.Text = _numberOfSeconds.ToString();
+            --_numberOfSeconds;
 
+            if (_numberOfSeconds == 0)
+            {
+                timerCountDown.Stop();
+                button1.Text = "Start";
+                timerDuration.Start();
+            }
         }
     }
 }
